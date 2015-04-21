6821dd1946f6dbb33262c2d2211f286551e44686
sam.medley <sam.medley@carsales.com.au>
2015-04-21 17:41:13 +1000

    Successfully reading RewriteTargets from database

--- Rainbow/getScarlettTargets.fsx
+++ Rainbow/getScarlettTargets.fsx
@@ -8,28 +8,38 @@ open System.Data.SqlClient
 // let bikesales = Rainbow.Portal.Bikesales
 // bikesales.PortalId
 
-let getResult sql getItemFromReader = 
+type RewriteTarget = { Id: int; Name: string; TemplateUrl: string; Location: string;}
+
+let getResult<'a> sql (getItemFromReader : SqlDataReader -> 'a) = 
     use connection = new SqlConnection(Rainbow.ScarlettDevConnectionString)
     let command = new SqlCommand(sql, connection)
     try
         connection.Open()
         use reader = command.ExecuteReader()
         printfn "Using the reader"
-        while reader.Read() do
-            getItemFromReader reader
-        1
+        let records =
+            [|
+                while reader.Read() do
+                    yield getItemFromReader reader                    
+            |]
+        records
     with
-        | :? Exception as ex -> 
+        | ex -> 
             printfn "Exception: %s" ex.Message
-            0
+            Array.empty<'a>
 
-let rewriteCallback sqlReader : SqlDataReader =
-    let id = sqlReader.[0].ToString()
-    let name = sqlReader.[1].ToString()
-    printfn "%s: %s" id name
+let parseRewriteTarget (sqlReader:SqlDataReader) =
+    { 
+        Id = Int32.Parse(sqlReader.[0].ToString()); 
+        Name = sqlReader.[1].ToString(); 
+        TemplateUrl = sqlReader.[2].ToString(); 
+        Location = sqlReader.[3].ToString() 
+    }    
 
-let result = getResult (String.Format("SELECT TOP 10 Id, Name FROM tbRewriteTargets WHERE PortalId = {0}", int Rainbow.Portal.Bikesales.PortalId)) rewriteCallback
+let bikesaleResults = getResult (String.Format("SELECT Id, Name, TemplateUrl, Location FROM tbRewriteTargets WHERE PortalId = {0}", int Rainbow.Portal.Bikesales.PortalId)) parseRewriteTarget
 
+for item in bikesaleResults do
+    printfn "%d: %s\n\t%s\n\t%s" item.Id item.Name item.TemplateUrl item.Location
 
 
 //printfn "%s" boatsales.PortalAlias
